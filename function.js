e1fa1f82f990f4a3d8f2ec6c76fd5fa476fa41bc
silvanakan <126418691+silvanakan@users.noreply.github.com>
2023-02-26 17:23:17 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,5 @@
+console.log ("Hola mundo");
+document.getElementById("boton").onclick= function(){
+    console.log ("intentando crear un evento en JS");
+    document.getElementById ("prueba").innerHTML="Responderé a la brevedad"
+}
